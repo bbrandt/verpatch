66d18d43aa7e2d8ece87db3cbe18bcaa5537bdf7
SND\ddbug_cp <SND\ddbug_cp@555e8bc2-ad3d-4a22-a156-a42dcab9bd6f>
2013-01-18 18:52:28 +0000

    copied from tag: v_1.0.9-codeproj
    git-svn-id: https://ddverpatch.svn.codeplex.com/svn/trunk@21694 555e8bc2-ad3d-4a22-a156-a42dcab9bd6f

---
+++ ver-self.cmd
@@ -0,0 +1,23 @@
+: Add version info resource with some strings and a help text resource file to verpatch.exe
+: (also kind of self test)
+
+: Run this in Release or Debug dir.
+:
+set _ver="1.0.9.22 [%date%]"
+set _s1=/s desc "PE version patcher tool" 
+set _s1=%_s1% /s copyright "(C) 1998-2011, pavel_a"
+set _s1=%_s1% /pv "1.0.0.1-CodeProject" 
+set _s2=/s private "%USERNAME%@%USERDOMAIN%"
+
+: Note: 64 is resource ID of help text, hardcoded in the program
+set _resfile=/rf #64 %~dp0\usage.txt
+
+: Run a copy of verpatch on itself:
+
+copy verpatch.exe v.exe || exit /b 1
+
+v.exe verpatch.exe /va %_ver% %_s1% %_s2% %_resfile% 
+
+@echo Errorlevel=%errorlevel%
+
+@rem del v.exe
